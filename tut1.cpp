303a413cfd323adfb3807494ecbe322fec09ca0d
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-21 23:09:22 +0530

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <typeinfo>
+using namespace std;
+
+class planet
+{
+public:
+    string name;
+    int earth;
+    int adder(int x, int y)
+    {
+        int add = x + y;
+        return add;
+    }
+};
+
+int main()
+{   
+
+    planet(earth);
+    planet(mars);
+    earth.name = "vyaom";
+    cout << earth.name << " " << earth.adder(54, 64) << " ";
+    int num = 0b1010;
+    int alpha = 0x00000c;
+    long double p = 0.5356635353463536353635;
+    std::string t = "jai";
+    std::cout << sizeof(p); // sizeof ( t ) ;
+    std::string prometheus;
+    // int projector;
+    // double nortonian;
+    // // std::cout << sizeof(nortonian);
+    // int super{3543};
+    // auto xterno{false};
+    // int xtream ;
+    // std::cout << typeid(xterno).name();
+    // std::cout << xterno;
+}
---
+++ tut2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int* a = &[3,4,5,3,5,9]
+    std::cout<<a[0]; 
+    // int a = 54;
+    // int *p = &a;
+    // int *q = p;
+    // int *x = q;
+    // std::cout << &a << " " << &p << " " << p << " " << &q << " " << q << " " << &x << " " << x;
+}
