f1ffd0846bfebac2c67535825d1b81603c991e35
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-23 23:22:43 +0530

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    auto a{356666666666666};
+    auto b{0.453345};
+    std::cout << sizeof(b)<< " ";
+    int var = 23425;
+    var = var + 1;
+    auto e{4};
+    e = -2;
+    std::cout << e;
+}
