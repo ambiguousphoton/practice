d37958e350b600604b0bdf9291e5062253d20f80
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-23 23:23:06 +0530

    Add files via upload

---
+++ tut.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    char c{'c'};
+    char a{65};
+    std::cout << a << "\n";
+    char val{65};
+    std::cout << static_cast<int>(val + 1) << "\n"
+              << static_cast<char>(val + 5) << "\n";
+    std::cout << sizeof(val);
+}
