fee7a9299c0619125451ad0b9610df422db3e709
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-25 22:48:32 +0530

    Add files via upload

---
+++ tut8.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+// #include <string>
+#include <cmath>
+
+int main()
+{
+
+    std::cout<<log(102);
+    // int d = 4;
+    // int c = 9;
+    // int b = abs(d-c);
+    // std::cout<<b;
+    // std::cout << exp(10);
+    int xi = sumer(34,53)
+    // int var = 4;
+    // int power = 5;
+    // int temp = 1;
+    // for (int i = 0; i < power; i++)
+    // {
+    //     temp = var * temp;
+    // }
+    // std::cout << temp;
+    // char a = 'A';
+    // cout << static_cast<int>(a)<<"\n";
+    // int b = static_cast<int>(a) + 1;
+    // cout << static_cast<char>(b);
+    // int x = 20371;
+    // char v[5] = {'v', 'y', 'o', 'a', 'm'};
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << v[i];
+    // }
+    // string v2 = "vikrant";
+    // cout << v2;
+    // cout << static_cast<char>(x);
+}
