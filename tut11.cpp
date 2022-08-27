067ab39243977488180b24ed08c3b9f8006dc0ab
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-27 11:01:16 +0530

    Add files via upload

---
+++ tut11.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+int main()
+{   const float radtodegree = 57.296;
+    long double temp = 0;
+    std::cout << std::setprecision(20);
+    long double values[5];
+    for (int i = 0; i < 5; i++)
+    {
+        values[i] = std::sin(temp / radtodegree );
+        temp += 30;
+    }
+    temp = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << "sin(" << temp << ") = " << values[i] << "\n";
+        temp += 30;
+        if (temp == 60){
+        std::cout << "sin(45) = "<<sin(45/radtodegree)<<"\n"; }
+        }
+}
