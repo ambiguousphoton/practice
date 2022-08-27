2a54493dce51ea98b3402177d4e1f28c0f683fb3
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-27 15:39:49 +0530

    Add files via upload

---
+++ tut13.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <typeinfo>
+int main()
+{
+    short int x, y;
+    x = 10;
+    y = 23;
+    char a{15};
+    char b{50};
+
+    std::cout << sizeof(x) << " " << sizeof(a) << "\n";
+    auto xy = x + y;
+    std::cout << a << " " << b << " " << 65 << "\n";
+    auto ab = a + b;
+    std::cout << xy << " " << sizeof(xy) << typeid(xy).name() << "\n";
+    std::cout << ab << " " << sizeof(ab) << typeid(ab).name() << "\n";
+    std::cout << static_cast<char>(ab);
+}
