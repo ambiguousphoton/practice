571e1888c08cead466e642e685a21dc90d375184
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-28 23:39:08 +0530

    Add files via upload

---
+++ tut16.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+    size_t j{};
+    
+    long y = 3410066600;
+    std::cout << sizeof(j) << " " << sizeof(y) << " " << sizeof(a) << "\n";
+    for (size_t i{}; i <= 4; ++i)
+    {
+        std::cout << i << "\n";
+    }
+    for (int i{}; i >= 0;)
+    {
+        std::cout << i << " ";
+        i *= 2 ;
+    }
+}
