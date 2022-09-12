9860557a33941b73cd39f4aa9268e58a19939b1f
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-09-12 23:54:40 +0530

    Update Tut17.cpp

--- Tut17.cpp
+++ Tut17.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 int main(){
 auto x = 4/5d;
+auto arr[2];
+int size;
+std::cin >> size;
 for(size_t i {}; i < size ; i++){
-std::cout << " " << x << " ";}
+std::cout << " " << x << " ";
+arr[i] = x;
+x = x/2;
+}
+  
 }
