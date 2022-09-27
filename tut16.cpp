ab8fac57f8465b6157a892c52f635ff93f964c6e
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-09-27 23:52:45 +0530

    Update tut16.cpp

--- tut16.cpp
+++ tut16.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <string>
 int main()
 {
     size_t j{};
+string jai = "jai shree ram";
     
     long y = 3410066600;
     std::cout << sizeof(j) << " " << sizeof(y) << " " << sizeof(a) << "\n";
     for (size_t i{}; i <= 4; ++i)
     {
         std::cout << i << "\n";
+        char v  = 'x'; 
     }
     for (int i{}; i >= 0;)
     {
